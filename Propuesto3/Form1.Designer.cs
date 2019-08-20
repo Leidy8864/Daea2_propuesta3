8336047b8ad0be470a1e2dbe19bbbb3fb7ee65bf
Leidy C_S <leidy.callupe@tecsup.edu.pe>
2019-08-19 20:45:28 -0500

    first commit

---
+++ Propuesto3/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Propuesto3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbxAnio = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbxMes = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgPedidos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgPedidos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbxAnio
+            // 
+            this.cbxAnio.FormattingEnabled = true;
+            this.cbxAnio.Location = new System.Drawing.Point(276, 23);
+            this.cbxAnio.Name = "cbxAnio";
+            this.cbxAnio.Size = new System.Drawing.Size(121, 21);
+            this.cbxAnio.TabIndex = 3;
+            this.cbxAnio.SelectionChangeCommitted += new System.EventHandler(this.cbxAnio_SelectionChangeCommitted);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "SELECCIONAR EL ANiO DEL PEDIDO";
+            // 
+            // cbxMes
+            // 
+            this.cbxMes.FormattingEnabled = true;
+            this.cbxMes.Location = new System.Drawing.Point(276, 66);
+            this.cbxMes.Name = "cbxMes";
+            this.cbxMes.Size = new System.Drawing.Size(121, 21);
+            this.cbxMes.TabIndex = 4;
+            this.cbxMes.SelectionChangeCommitted += new System.EventHandler(this.cbxMes_SelectionChangeCommitted);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "SELECCIONAR EL MES DEL PEDIDO";
+            // 
+            // dgPedidos
+            // 
+            this.dgPedidos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgPedidos.Location = new System.Drawing.Point(65, 110);
+            this.dgPedidos.Name = "dgPedidos";
+            this.dgPedidos.Size = new System.Drawing.Size(537, 289);
+            this.dgPedidos.TabIndex = 6;
+            this.dgPedidos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgPedidos_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgPedidos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbxMes);
+            this.Controls.Add(this.cbxAnio);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgPedidos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbxAnio;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbxMes;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgPedidos;
+    }
+}
+
---
+++ Propuesto3/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Propuesto3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dgPedidos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["kobu"].ConnectionString);
+
+        public void ListaAnios()
+        {
+            using (SqlCommand cmd = new SqlCommand("Usp_ListaAnios", cn))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter())
+                {
+                    da.SelectCommand = cmd;
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    using (DataSet df = new DataSet())
+                    {
+                        da.Fill(df, "ListaAnios");
+                        cbxAnio.DataSource = df.Tables["ListaAnios"];
+                        cbxAnio.DisplayMember = "Anios";
+                        cbxAnio.ValueMember = "Anios";
+                    }
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListaAnios();
+        }
+
+        private void cbxAnio_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            using (SqlCommand cmd = new SqlCommand("Usp_ListaMeses", cn))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter())
+                {
+                    da.SelectCommand = cmd;
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    da.SelectCommand.Parameters.AddWithValue("@anio", cbxAnio.SelectedValue);
+                    using (DataSet df = new DataSet())
+                    {
+                        da.Fill(df, "ListaMeses");
+                        cbxMes.DataSource = df.Tables["ListaMeses"];
+                        cbxMes.DisplayMember = "Mes";
+                        cbxMes.ValueMember = "Mes";
+                    }
+                }
+            }
+
+        }
+
+        private void cbxMes_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            using (SqlCommand cmd = new SqlCommand("Usp_Detalle_Ped_Mes_Anio", cn))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter())
+                {
+                    da.SelectCommand = cmd;
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    da.SelectCommand.Parameters.AddWithValue("@anio", cbxAnio.SelectedValue);
+                    da.SelectCommand.Parameters.AddWithValue("@mes", cbxMes.SelectedValue);
+                    using (DataSet df = new DataSet())
+                    {
+                        da.Fill(df, "Pedidos");
+                        dgPedidos.DataSource = df.Tables["Pedidos"];
+                    }
+                }
+            }
+        }
+    }
+}
